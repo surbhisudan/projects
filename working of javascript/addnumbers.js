cace23248c3b64474a70dff52b2e82d5c94d66e3
surbhisudan <121334042+surbhisudan@users.noreply.github.com>
2023-04-07 16:59:29 +0530

    Add files via upload

---
+++ working of javascript/addnumbers.js
@@ -0,0 +1,9 @@
+
+
+let res=addNumbers(3,4);
+console.log(res);
+
+function addNumbers(a,b){
+    let sum=a+b;
+    return sum;
+}
---
+++ working of javascript/addusingvar.js
@@ -0,0 +1,7 @@
+var res=addNumbers(9,4);
+console.log(res);
+
+function addNumbers(a,b){
+    var sum=a+b;
+    return sum;
+}
